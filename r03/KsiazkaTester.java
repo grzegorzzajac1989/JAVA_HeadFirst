8761993dc01482841aedc6658807a92f39626ff2
grzegorzzajac1989 <zajacgrzegorz1989@gmail.com>
2017-02-04 21:22:20 +0100

    Add solutions to r03/01

---
+++ r03/KsiazkaTester.java
@@ -0,0 +1,28 @@
+class Ksiazka {
+  String tytul;
+  String autor;
+}
+ 
+class KsiazkaTester {
+  public static void main(String[] args) {
+    
+    Ksiazka[] mojeKsiazki = new Ksiazka[3]; 
+    int x = 0;
+    mojeKsiazki[0] = new Ksiazka(); // stworzenie obiektow
+    mojeKsiazki[1] = new Ksiazka();
+    mojeKsiazki[2] = new Ksiazka();
+    mojeKsiazki[0].tytul = "Czterej koderzy i Java"; // odwolanie
+    mojeKsiazki[1].tytul = "Java nocy letniej";
+    mojeKsiazki[2].tytul = "Java. Receptury";
+    mojeKsiazki[0].autor = "janek";
+    mojeKsiazki[1].autor = "wilhelm";
+    mojeKsiazki[2].autor = "ian";
+
+    while (x < 3) {
+      System.out.print(mojeKsiazki[x].tytul);
+      System.out.print(", autor ");
+      System.out.println(mojeKsiazki[x].autor);
+      x = x + 1;
+    }
+  }
+}
